f74b583748744b36d20fcd937c390c324b67a1cd
Alice Bryer <alicebryer123@gmail.com>
2022-09-12 13:32:28 +0100

    intern questions added

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const promptUser = () => {
         addManager();
       }
       if (answer.action === "add Intern") {
-        console.log("adding intern");
+        addIntern();
       }
       if (answer.action === "add Engineer") {
         console.log("adding engineer");
@@ -66,6 +66,98 @@ const addManager = () => {
       }
       if (answer.action === "add Intern") {
         console.log("adding intern");
+      }
+      if (answer.action === "add Engineer") {
+        console.log("adding engineer");
+      }
+      if (answer.action === "Build website") {
+        console.log("Building website");
+      }
+    });
+};
+
+const addIntern = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the interns name? ",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter the id of the intern ",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the interns email address? ",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What school do they attend? ",
+      },
+      {
+        type: "list",
+        name: "action",
+        message: "What would you like to do now?",
+        choices: ["add Manager", "add Intern", "add Engineer", "Build website"],
+      },
+    ])
+    .then((answer) => {
+      if (answer.action === "add Manager") {
+        addManager();
+      }
+      if (answer.action === "add Intern") {
+        addIntern();
+      }
+      if (answer.action === "add Engineer") {
+        console.log("adding engineer");
+      }
+      if (answer.action === "Build website") {
+        console.log("Building website");
+      }
+    });
+};
+
+const addIntern = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the interns name? ",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter the id of the intern ",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the interns email address? ",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What school do they attend? ",
+      },
+      {
+        type: "list",
+        name: "action",
+        message: "What would you like to do now?",
+        choices: ["add Manager", "add Intern", "add Engineer", "Build website"],
+      },
+    ])
+    .then((answer) => {
+      if (answer.action === "add Manager") {
+        addManager();
+      }
+      if (answer.action === "add Intern") {
+        addIntern();
       }
       if (answer.action === "add Engineer") {
         console.log("adding engineer");
