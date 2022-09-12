916719fbfcb7d9d329270bccfcbc2f4ff4e7d7b5
Alice Bryer <alicebryer123@gmail.com>
2022-09-12 13:28:58 +0100

    manager questions done

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const promptUser = () => {
     ])
     .then((answer) => {
       if (answer.action === "add Manager") {
-        console.log("adding manager");
+        addManager();
       }
       if (answer.action === "add Intern") {
         console.log("adding intern");
@@ -29,3 +29,49 @@ const promptUser = () => {
 };
 
 promptUser();
+
+const addManager = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the managers name? ",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter the id of the manager ",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the managers email address? ",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the office number? ",
+      },
+      {
+        type: "list",
+        name: "action",
+        message: "What would you like to do now?",
+        choices: ["add Manager", "add Intern", "add Engineer", "Build website"],
+      },
+    ])
+    .then((answer) => {
+      if (answer.action === "add Manager") {
+        addManager();
+      }
+      if (answer.action === "add Intern") {
+        console.log("adding intern");
+      }
+      if (answer.action === "add Engineer") {
+        console.log("adding engineer");
+      }
+      if (answer.action === "Build website") {
+        console.log("Building website");
+      }
+    });
+};
