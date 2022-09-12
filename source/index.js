4d4e7de587279f7732b2301d7321887f1398d301
Alice Bryer <alicebryer123@gmail.com>
2022-09-12 03:19:43 +0100

    deleted to start again!!!!

--- source/index.js
+++
@@ -1,6 +0,0 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-
-const Engineer = require("./library/engineer");
-const intern = require("./library/intern");
-const manager = require("./library/manager");
